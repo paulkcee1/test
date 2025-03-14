dd463ae0e4b9f2b2affa81cf97c8e1f9016e8e2e
paul <paul@paulkcee.plus.com>
2025-03-14 06:46:37 +0000

    Update 14-03-25

---
+++ funky_code.py
@@ -0,0 +1,31 @@
+
+char = "#"
+char2 = "@"
+
+print(f" {char*4}")
+print(f"{char2}")
+print(f"{char2}")
+print(f"{char2}")
+print(f" {char*4}")
+print("")
+
+print(f" {char*4}")
+print(f"{char2}    {char}")
+print(f"{char2}    {char}")
+print(f"{char2}    {char}")
+print(f" {char*4}")
+print("")
+
+print(f"{char*3}")
+print(f"{char}  {char}")
+print(f"{char}   {char}")
+print(f"{char}  {char}")
+print(f"{char*3}")
+print("")
+
+print(f"{char*5}")
+print(f"{char}")
+print(f"{char*5}")
+print(f"{char}")
+print(f"{char*5}")
+
